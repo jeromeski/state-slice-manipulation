57b46cac68370dffee6630027a0778eea8f21b61
Jerome Gacoscosim <40538528+jeromeski@users.noreply.github.com>
2022-07-07 13:59:32 +0800

    update

--- src/components/cart-table/index.js
+++ src/components/cart-table/index.js
@@ -1,3 +1,21 @@
+import { CartTableWrap } from "./cart-table-style";
+
 const CartTable = () => {
-  return <CartTableWrap></CartTableWrap>;
+  return (
+    <CartTableWrap>
+      <table>
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </CartTableWrap>
+  );
 };
+
+export default CartTable;
